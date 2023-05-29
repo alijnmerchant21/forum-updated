34d50b54599721b17b4787a49871ed423a1ef252
Aliasgar Merchant <amercha1@stevens.edu>
2023-05-29 02:00:34 +0100

    Major refactor of db.go
    Added: Init, Commit, NewDB, GetDB, CreateUser, FindUserByName
    
    Removed GetMessagesByPubKey, GetRawTxsByPubKeys, GetByPubKey, SaveUser

--- model/db.go
+++ model/db.go
@@ -1,129 +1,97 @@
 package model
 
 import (
-	"bytes"
-	"compress/flate"
 	"encoding/json"
-	"sync"
 
-	"github.com/cometbft/cometbft/crypto/ed25519"
-	"github.com/dgraph-io/badger"
-	"github.com/timshannon/badgerhold"
+	"github.com/dgraph-io/badger/v3"
+	"github.com/pkg/errors"
 )
 
-var bufPool = sync.Pool{New: func() interface{} { return &bytes.Buffer{} }}
-
 type DB struct {
-	store *badgerhold.Store
+	db *badger.DB
 }
 
-func New(dbPath string) (*DB, error) {
-	store, err := badgerhold.Open(
-		badgerhold.Options{
-			Encoder: func(v interface{}) ([]byte, error) {
-				jby, err := json.Marshal(v)
-				if err != nil {
-					return nil, err
-				}
+func (db *DB) Init(database *badger.DB) {
+	db.db = database
+}
 
-				buf := bufPool.Get().(*bytes.Buffer)
-				defer bufPool.Put(buf)
-				buf.Reset()
-				gz, err := flate.NewWriter(buf, 5)
-				if err != nil {
-					return nil, err
-				}
-				defer gz.Close()
+func (db *DB) Commit() error {
+	return db.db.Update(func(txn *badger.Txn) error {
+		return txn.Commit()
+	})
+}
 
-				if _, err := gz.Write(jby); err != nil {
-					return nil, err
-				}
-
-				return buf.Bytes(), nil
-			},
-			Decoder: func(in []byte, val interface{}) error {
-				buf := bufPool.Get().(*bytes.Buffer)
-				defer bufPool.Put(buf)
-				buf.Reset()
-
-				gz := flate.NewReader(bytes.NewReader(in))
-				defer gz.Close()
-
-				if _, err := buf.ReadFrom(gz); err != nil {
-					return err
-				}
-				return json.Unmarshal(buf.Bytes(), val)
-			},
-			Options: badger.DefaultOptions(dbPath),
-		},
-	)
+func NewDB(dbPath string) (*DB, error) {
+	// Open badger DB
+	opts := badger.DefaultOptions(dbPath)
+	db, err := badger.Open(opts)
 	if err != nil {
 		return nil, err
 	}
 
-	return &DB{
-		store: store,
-	}, nil
+	// Create a new DB instance and initialize with badger DB
+	dbInstance := &DB{}
+	dbInstance.Init(db)
+
+	return dbInstance, nil
 }
 
-func (db *DB) Close() error { return db.store.Close() }
-
-func (db *DB) CreateUser(u *User) error {
-	return db.store.Insert(u.PubKey, u)
+func (db *DB) GetDB() *badger.DB {
+	return db.db
 }
 
-func (db *DB) SaveUser(u *User) error {
-	return db.store.Update(u.PubKey, u)
-}
-
-func (db *DB) FindUser(key ed25519.PubKey) (*User, error) {
-	var user User
-	err := db.store.Get(key, &user)
-	if err != nil {
-		return nil, err
-	}
-	return &user, nil
-}
-
-func (m *DB) GetByPubKey(pubkey ed25519.PubKey, msg *Message) error {
-	err := m.store.Get(pubkey.String(), msg)
-	if err != nil {
+func (db *DB) CreateUser(user *User) error {
+	// Check if the user already exists
+	err := db.db.View(func(txn *badger.Txn) error {
+		_, err := txn.Get([]byte(user.Name))
 		return err
+	})
+	if err == nil {
+		return errors.New("user already exists")
 	}
+
+	// Save the user to the database
+	err = db.db.Update(func(txn *badger.Txn) error {
+		userBytes, err := json.Marshal(user)
+		if err != nil {
+			return errors.Wrap(err, "failed to marshal user to JSON")
+		}
+		err = txn.Set([]byte(user.Name), userBytes)
+		if err != nil {
+			return errors.Wrap(err, "failed to save user to database")
+		}
+		return nil
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to create user")
+	}
+
 	return nil
 }
 
-func (db *DB) GetRawTxsByPubKeys(pubkeys []ed25519.PubKey) ([][]byte, error) {
-	rawTxs := make([][]byte, len(pubkeys))
-	for i, pubkey := range pubkeys {
-		var txBytes []byte
-		if err := db.store.Get(pubkey.String(), &txBytes); err != nil {
-			return nil, err
+func (db *DB) FindUserByName(name string) (*User, error) {
+	// Read the user from the database
+	var user User
+	err := db.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(name))
+		if err != nil {
+			return err
 		}
-
-		buf := bytes.NewBuffer(txBytes)
-		gz := flate.NewReader(buf)
-
-		defer gz.Close()
-
-		var rawTx bytes.Buffer
-		if _, err := rawTx.ReadFrom(gz); err != nil {
-			return nil, err
+		err = item.Value(func(val []byte) error {
+			return json.Unmarshal(val, &user)
+		})
+		if err != nil {
+			return err
 		}
-
-		rawTxs[i] = rawTx.Bytes()
+		return nil
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to find user")
 	}
 
-	return rawTxs, nil
+	return &user, nil
 }
 
-func (db *DB) GetMessagesByPubKey(pubkey ed25519.PubKey) ([]*Message, error) {
-	// Query the database for messages with the given public key
-	var messages []*Message
-	err := db.store.Find(&messages, badgerhold.Where("PubKey").Eq(pubkey.String()))
-	if err != nil {
-		return nil, err
-	}
-
-	return messages, nil
+func (db *DB) Close() error {
+	return db.db.Close()
 }
